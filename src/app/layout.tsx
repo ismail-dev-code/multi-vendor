5980031ec332f12bc23b9b678ab9dd37534e4386
ismail-dev-code <hm.ismail772@gmail.com>
2025-09-20 10:05:22 +0600

    update: text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// Load DM Sans font
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
-    <div>
-   <Button variant="destructive">
-    Hello
-   </Button>
+    <div className="p-5 text-center space-y-3 border-2">
+      <div>
+        <Button>I am a Button</Button>
+      </div>
+      <div>
+        <progress value={50}></progress>
+      </div>
+      <div>
+        <input placeholder="I am a input" />
+      </div>
+      <div>
+        <textarea value="I am text area"></textarea>
+      </div>
+      <div>
+        <Checkbox></Checkbox>{" "}
+      </div>
     </div>
   );
 }
